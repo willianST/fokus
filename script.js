621b083256632209efd1ea33c2f4271a5529aa94
Willian Teixeira <ws.teixeira@outlook.com>
2026-06-16 17:21:13 -0300

    feat: Finaliza troca de texto

--- script.js
+++ script.js
@@ -3,10 +3,27 @@ const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title")
 
 function alteraContexto(contexto) {
     html.setAttribute("data-contexto", contexto);
     banner.setAttribute("src", `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br />
+          <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;  
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br />
+          <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície.<br />
+          <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+            break;
+        default:
+            break;
+    }
 }
 
 focoBt.addEventListener("click", () => {
