b5f76f55b7f89c2ad7b89b445b077e2a666eef7a
Willian Teixeira <ws.teixeira@outlook.com>
2026-06-17 15:11:32 -0300

    feat: Adiciona audio

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
+const musicaImput = document.querySelector("#alternar-musica");
+const musica = new Audio("/sons/luna-rise-part-one.mp3");
+musica.loop = true;
+
+musicaImput.addEventListener("change", () => {
+    if(musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+})
 
 function alteraContexto(contexto) {
     botoes.forEach(function(botao) {
