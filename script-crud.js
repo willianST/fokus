a42cb367203de8179cae7290870aa41a3a074070
Willian Teixeira <ws.teixeira@outlook.com>
2026-06-22 16:25:38 -0300

    feat: cria lista de tarefas

--- script-crud.js
+++ script-crud.js
@@ -1,12 +1,43 @@
 const adicionarTarefaBt = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
-const textArea = document.querySelector(".app__form-textarea")
+const textArea = document.querySelector(".app__form-textarea");
+const ulTarefas = document.querySelector(".app__section-task-list");
 
-const tarefas = [];
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+function criaElementoTarefa(tarefa) {
+    const li = document.createElement("li");
+    li.classList.add("app__section-task-list-item");
+
+    const svg = document.createElement("svg");
+    svg.innerHTML = `
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    `
+
+    const p = document.createElement("p");
+    p.textContent = tarefa.descricao;
+    p.classList.add("app__section-task-list-item-description");
+
+    const button = document.createElement("button");
+    button.classList.add("app_button-edit");
+    
+    const imgButton = document.createElement("img");
+    imgButton.setAttribute("src", "/imagens/edit.png");
+    button.append(imgButton);
+
+    li.append(svg);
+    li.append(p);
+    li.append(button);
+
+    return li
+};
 
 adicionarTarefaBt.addEventListener("click", () => {
     formAdicionarTarefa.classList.toggle("hidden");
-})
+});
 
 formAdicionarTarefa.addEventListener("submit", (evento) => {
     evento.preventDefault();
@@ -15,4 +46,9 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
     };
     tarefas.push(tarefa)
     localStorage.setItem("tarefas", JSON.stringify(tarefas));
-})
+});
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criaElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
+});
