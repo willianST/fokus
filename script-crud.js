4d00c7b84c62a8776279228293d16caa5cb0be59
Willian Teixeira <ws.teixeira@outlook.com>
2026-06-22 16:30:32 -0300

    feat: mostra tarefa e esconde formulario

--- script-crud.js
+++ script-crud.js
@@ -16,14 +16,13 @@ function criaElementoTarefa(tarefa) {
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
     `
-
     const p = document.createElement("p");
     p.textContent = tarefa.descricao;
     p.classList.add("app__section-task-list-item-description");
 
     const button = document.createElement("button");
     button.classList.add("app_button-edit");
-    
+
     const imgButton = document.createElement("img");
     imgButton.setAttribute("src", "/imagens/edit.png");
     button.append(imgButton);
@@ -45,7 +44,11 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
         descricao: textArea.value
     };
     tarefas.push(tarefa)
+    const elementoTarefa = criaElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
     localStorage.setItem("tarefas", JSON.stringify(tarefas));
+    textArea.value = "";
+    formAdicionarTarefa.classList.add("hidden");
 });
 
 tarefas.forEach(tarefa => {
