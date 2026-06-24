bc19618a515ad21475455f3ea2ef55d1e0a54b19
Willian Teixeira <ws.teixeira@outlook.com>
2026-06-24 14:33:21 -0300

    feat: cria funcao para remover tarefas concluidas

--- script-crud.js
+++ script-crud.js
@@ -5,7 +5,9 @@ const ulTarefas = document.querySelector(".app__section-task-list");
 const btCancelar = document.querySelector(".app__form-footer__button--cancel");
 const paragrafoDescricaoTarefa = document.querySelector(".app__section-active-task-description");
 
-const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+const btnRemoverConcluidas = document.querySelector("#btn-remover-concluidas");
+
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 let tarefaSelecionada = null;
 let liTarefaSelecionada = null;
 
@@ -116,3 +118,12 @@ document.addEventListener("focoFinalizado", () => {
         atualizarTarefas();
     }
 })
+
+btnRemoverConcluidas.onclick = () => {
+    const seletor = ".app__section-task-list-item-complete";
+    document.querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove()
+    })
+    tarefas = tarefas.filter(tarefa => !tarefa.completa)
+    atualizarTarefas()
+}
